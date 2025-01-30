dd6e339ffd030a2ab2bb65571bd1eee32e022aa0
Cesar Bermudez <cesar.bermudez.sierra@gmail.com>
2025-01-29 22:22:21 -0500

    Working on env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,16 @@
 export const environment = {
   OPEN_AI: {
-    API_KEY: '',
-    ORGANIZATION: '',
-    PROJECT: '',
-    MODEL: '',
-    ROLE: '',
+    API_KEY: import.meta.env['NG_APP_OPEN_AI_API_KEY'],
+    ORGANIZATION: import.meta.env['NG_APP_OPEN_AI_ORGANIZATION'],
+    PROJECT: import.meta.env['NG_APP_OPEN_AI_PROJECT'],
+    MODEL: import.meta.env['NG_APP_OPEN_AI_MODEL'],
+    ROLE: import.meta.env['NG_APP_OPEN_AI_ROLE'],
   },
   SUPABASE: {
-    PROJECT_URL: '',
-    API_KEY: '',
+    PROJECT_URL: import.meta.env['NG_APP_SUPABASE_PROJECT_URL'],
+    API_KEY: import.meta.env['NG_APP_SUPABASE_API_KEY'],
+  },
+  TEMPLATE: {
+    URL: import.meta.env['NG_APP_TEMPLATE_URL'],
   },
 };
